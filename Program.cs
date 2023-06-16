f4c4f20d68e54f6654aa196dc16c78c431b5a63b
Jakub146480 <128227074+Jakub146480@users.noreply.github.com>
2023-06-17 00:55:44 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj podstawę: ");
+        double baseNumber = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj wykładnik: ");
+        int exponent = Convert.ToInt32(Console.ReadLine());
+
+        double result = Power(baseNumber, exponent);
+        Console.WriteLine($"{baseNumber}^{exponent} = {result}");
+    }
+
+    static double Power(double baseNumber, int exponent)
+    {
+        double result = 1.0;
+        for (int i = 0; i < exponent; i++)
+        {
+            result *= baseNumber;
+        }
+        return result;
+    }
+}
